cb90c23ae1ad5f6ca99a395b23d2070f41c77069
G <gerardo.arriaga@uabc.edu.mx>
2020-08-30 17:41:04 -0500

    Changes

--- tarea1.c
+++ tarea1.c
@@ -3,5 +3,41 @@
 //#include<
 
 
+int getValue()
+{
+    int value = 0; 
+
+    printf("\n\r");
+    printf("Please insert the value to calculate the MCD:\n\r");
+    scanf("%d", &value);
+    printf("\n\r");
+
+    return value;
+}
+
+void printValue(int value)
+{
+    printf("\n\r");
+    printf("Given value is:\n\r %d", value);
+    printf("\n\r");
+}
+
+void calculateMCD()
+{
+    int a = 0, b = 0, q = 0, r = 0; 
+
+    a = getValue();
+
+    b = getValue();
+
+    r = a%b;
+
+    printValue(r);
+
+}
 
 
+void main()
+{
+    calculateMCD();
+}
