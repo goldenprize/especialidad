a3916b0e1dcdc1feb1fae8aa7f087f714e0fb663
G <gerardo.arriaga@uabc.edu.mx>
2020-09-10 13:28:42 -0500

    Added max limit for random number

--- random_number_gen.c
+++ random_number_gen.c
@@ -1,18 +1,53 @@
+/*=============================================================================
+ |  Assignment: Random Number Generator
+ |
+ |  Author:     Gerardo Arriaga Ramirez
+
+ |  Language:   C
+
+ |  To Compile: The compiler used was the gcc compiler. 
+                In order to compile it, first install the gcc compiler. 
+                Open a cmd, in it type: 
+                    gcc random_number_gen.c -o random_number_gen
+                It should Generate an .exe file
+ |
+ |  Class:      Ingenieria de Software en Ambientes Embebidos
+
+ |  Instructor: Luis Julian Dominguez
+ 
+ |  Due Date:   10/9/20
+ *===========================================================================*/
 
 #include <stdio.h>
 #include <stdlib.h> 
 #include <time.h>
 
-int GenerateRandom()
+int GetMaxNumber()
+{
+    int value = 0; 
+
+    printf("\n\r");
+    printf("Please Insert the Max random number that you want to generate:\n\r");
+    scanf("%d", &value);
+    printf("\n\r");
+
+    return value;
+}
+
+int GenerateRandom(int limit)
 {
     srand(time(0)); 
 
-    return rand();
+    return rand() % limit;
 }
 
 void PrintRandomNumber()
 {
-    printf("Random number is: %d", GenerateRandom() );
+    int maxValue = 0;
+
+    maxValue = GetMaxNumber();
+
+    printf("Random number is: %d", GenerateRandom(maxValue) );
 }
 
 void main()
