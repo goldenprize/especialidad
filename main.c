5abd1173db350e82a076a57a857b4e0237b19704
G <gerardo.arriaga@uabc.edu.mx>
2020-10-13 23:01:55 -0500

    Draft of menu for main function

--- main.c
+++ main.c
@@ -3,12 +3,22 @@
 
 void main()
 {
-	uint8_t value = 0;
+    uint8_t value = 0;
 
-    uint8_t salsa = 0;
+    printf("\nThis program is designed to encrypt files through:\n");
+    printf("\tChacha algorithm\n");
+    printf("\tSalsa20 algorithm\n");
+    printf("\tTrivium algorithm\n");
+    printf("\t=================\n");
 
 	while((value = getchar()) != '0')
     {
+        printf("\nOptions:\n");
+        printf("\tType 'c' to use the Chacha algorithm\n");
+        printf("\tType 's' to use the Salsa20 algorithm\n");
+        printf("\tType 't' to use the Trivium algorithm\n");
+        printf("\nPress 0 to exit\n");
+        
         switch(value)
         {
 			case 't': 
